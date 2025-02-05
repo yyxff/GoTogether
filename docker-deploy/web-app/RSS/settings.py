122bb8fb6fb02975fae1042525cae5aca2851125
yyxff <salmontwosday@gmail.com>
2025-02-05 16:55:28 -0500

    updated env, now can automatically switch DB_HOST for local running and docker running

--- docker-deploy/web-app/RSS/settings.py
+++ docker-deploy/web-app/RSS/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,7 +82,7 @@ DATABASES = {
         "NAME": "postgres",
         "USER": "postgres",
         "PASSWORD": "password",
-        "HOST": "db",
+        "HOST": os.getenv('DB_HOST', '127.0.0.1'),
         "PORT": "5432",
     }
 }
