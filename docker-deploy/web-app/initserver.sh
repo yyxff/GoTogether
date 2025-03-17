eb626294e6a8184380c8d85e933c285f79b4bb48
yyxff <salmontwosday@gmail.com>
2025-03-16 21:20:15 -0400

    added nginx static cache and fixed log file problem

--- docker-deploy/web-app/initserver.sh
+++ docker-deploy/web-app/initserver.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+mkdir -p /code/logs
+test -f /code/logs/django_danger.log || touch /code/logs/django_danger.log
+test -f /code/logs/django_debug.log || touch /code/logs/django_debug.log
+test -f /code/logs/django_info.log || touch /code/logs/django_info.log
+chown nobody:nogroup /code/logs/*.log
+chmod 666 /code/logs/*.log
+
+
 python3 manage.py makemigrations
 python3 manage.py migrate
 res="$?"
--- docker-deploy/web-app/runserver.sh
+++ docker-deploy/web-app/runserver.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+mkdir -p /code/logs
+test -f /code/logs/django_danger.log || touch /code/logs/django_danger.log
+test -f /code/logs/django_debug.log || touch /code/logs/django_debug.log
+test -f /code/logs/django_info.log || touch /code/logs/django_info.log
+chown nobody:nogroup /code/logs/*.log
+chmod 666 /code/logs/*.log
+
 while [ "1"=="1" ]
 do
     python3 manage.py runserver 0.0.0.0:8000
